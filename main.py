fc06646dd7d026f924d9794beca85dd2de87b568
parthsit@gmail.com <parthsit@gmail.com>
2020-05-02 15:35:02 +0530

    second

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class MyGrid(GridLayout):
+    def __init__(self,**kwargs):
+        super(MyGrid,self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text="Name :"))
+        self.name = TextInput(multiline=False)
+        self.add_widget(self.name)
+
+        self.add_widget(Label(text="Last Name :"))
+        self.last_name = TextInput(multiline=False)
+        self.add_widget(self.last_name)
+
+        self.add_widget(Label(text="Email :"))
+        self.email = TextInput(multiline=False)
+        self.add_widget(self.email)
 
 class MainApp(App):
     def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
-
-        return label
+        return MyGrid()
 
 if __name__ == '__main__':
     app = MainApp()
